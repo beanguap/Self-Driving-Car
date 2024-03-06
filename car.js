da01b37d0c7141a8ed4c4905b1f4df02670fcab7
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-03-06 14:46:59 -0500

    network

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-    constructor(x,y,width,height) {
+    constructor(x,y,width,height, controlType, maxSpeed = 3) {
         this.x=x;
         this.y=y;
         this.width=width;
@@ -7,19 +7,82 @@ class Car {
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
         this.angle = 0;
+        this.damaged=false;
 
+        this.useBrain=controlType=="AI";
+
+        if(controlType != "DUMMY"){
         this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        this.brain = new NeuralNetwork(
+            [this.sensor.rayCount,6,4]
+        );
+    }
+        this.controls = new Controls(controlType);
     }
 
 
 // Defining acceleration/deceleration of car
-update(roadBoarders) {
+update(roadBoarders, traffic) {
+    if(!this.damaged) {
     this.#move();
-    this.sensor.update(roadBoarders);
+    this.polygon = this.#createPolygon();
+    this.damaged=this.#assessDamage(roadBoarders, traffic)
+    }
+    if(this.sensor) {
+    this.sensor.update(roadBoarders, traffic);
+    const offsets = this.sensor.readings.map(
+        s=>s==null?0:1-s
+    );
+    const outputs = NeuralNetwork.feedForward(offsets,this.brain)
+    console.log(outputs);
+
+    if(this.useBrain){
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+        }
+    }
+}
+
+#assessDamage(roadBoarders, traffic) {
+    for(let i=0; i < roadBoarders.length; i++) {
+        if(polysIntersect(this.polygon,roadBoarders[i])){
+            return true;
+        }
+    }
+    for(let i=0; i < traffic.length; i++) {
+        if(polysIntersect(this.polygon,traffic[i].polygon)){
+            return true;
+        }
+    }
+    return false;
+}
+
+#createPolygon() {
+    const points = [];
+    const rad=Math.hypot(this.width,this.height)/2;
+    const alpha = Math.atan2(this.width,this.height);
+    points.push({
+        x:this.x-Math.sin(this.angle-alpha)*rad,
+        y:this.y-Math.cos(this.angle-alpha)*rad
+    });
+    points.push({
+        x:this.x-Math.sin(this.angle+alpha)*rad,
+        y:this.y-Math.cos(this.angle+alpha)*rad
+    });
+    points.push({
+        x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+        y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+    });
+    points.push({
+        x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+        y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+    });
+    return points;
 }
 
 #move(){
@@ -65,22 +128,21 @@ update(roadBoarders) {
 
 // Defining car position
 // ctx = context 
-draw(ctx) {
-    ctx.save();
-    ctx.translate(this.x,this.y);
-    ctx.rotate(-this.angle);
+draw(ctx,color) {
+    if(this.damaged) {
+        ctx.fillStyle="gray";
+    } else {
+        ctx.fillStyle=color;
+    }
     ctx.beginPath();
-    // Drawing car shape: X = center
-    ctx.rect(
-        -this.width/2,
-        -this.height/2,
-        this.width,
-        this.height
-    );
+    ctx.moveTo(this.polygon[0].x,this.polygon[0].y)
+    for(let i=1; i < this.polygon.length; i++) {
+        ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+    }
     ctx.fill();
 
-    ctx.restore();
-
+    if(this.sensor) {
     this.sensor.draw(ctx);
+    }
     }
 }
